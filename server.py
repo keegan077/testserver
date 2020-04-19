9cb21e6e2f62638d5c8c3a0ad9c871c5490a0931
Keegan <keeganbrown@hotmail.com>
2020-04-19 11:11:41 +0200

    Testing Jinja templates

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello world!, this is a second test message"
 
-@app.route('/helloworld')
-def test():
-    return "Another test!"
+@app.route('/hello')
+@app.route('/hello/<name>')
+def test(name=None):
+    return render_template("helloworld.html", name=name)
 
