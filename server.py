86b637e51a5f28b4fd85270bd030811ece87049c
Keegan <keeganbrown@hotmail.com>
2020-04-19 10:52:32 +0200

    Testing app routing

--- server.py
+++ server.py
@@ -3,4 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello world!"
+    return "Hello world!, this is a second test message"
+
+@app.route('/helloworld')
+def test():
+    return "Another test!"
+
